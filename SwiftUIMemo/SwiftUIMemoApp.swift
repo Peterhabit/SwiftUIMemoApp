a789d057efe2a53cce0af12fb90b6c99321fbc29
Camel-Milk <unicon1218@gmail.com>
2023-04-19 22:16:20 +0900

    Initial Commit

---
+++ SwiftUIMemo/SwiftUIMemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIMemoApp.swift
+//  SwiftUIMemo
+//
+//  Created by 소프트웨어컴퓨터 on 2023/04/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMemoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
