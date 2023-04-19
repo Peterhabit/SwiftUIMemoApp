ba66b519a66b125dbd2713e4e90343662974d81b
Camel-Milk <unicon1218@gmail.com>
2023-04-19 22:27:21 +0900

    init project

---
+++ SwiftUIMemo/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainListView.swift
+//  SwiftUIMemo
+//
+//  Created by 소프트웨어컴퓨터 on 2023/04/19.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+    }
+}
